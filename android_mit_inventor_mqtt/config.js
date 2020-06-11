42493c5b871e3847e35d18eff0458231c6448b79
calyerm <mike.calyer@yahoo.com>
2020-06-11 14:20:15 -0700

    Add files via upload

---
+++ android_mit_inventor_mqtt/config.js
@@ -0,0 +1,7 @@
+host  = "io.adafruit.com";	// hostname or IP address
+port  = 443;
+topic = 'mikec/feeds/temperature';		// topic to subscribe to
+useTLS   = true;
+username = "mikec";
+password = "8e739fae91b6472d93b4bee76e2eaa1c";
+cleansession = true;
